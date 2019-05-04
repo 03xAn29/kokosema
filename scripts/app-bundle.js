0c9bbdd4850281c588d9fa42c71a2e55c592cc30
daijiro obata <dobata@task-do.co.jp>
2019-05-04 17:56:16 +0900

    プロジェクトのフラット化。CSSをSASSに変更

---
+++ scripts/app-bundle.js
@@ -0,0 +1,48 @@
+define('resources',['resources/index'],function(m){return m;});;
+define('app',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    var App = (function () {
+        function App() {
+            this.message = 'Hello World!';
+        }
+        return App;
+    }());
+    exports.App = App;
+});
+;
+define('app.html!text',[],function(){return "<template>\n  <h1>${message}</h1>\n</template>\n";});;
+define('environment',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.default = {
+        debug: true,
+        testing: true
+    };
+});
+;
+define('main',["require", "exports", "./environment", "core-js/stable"], function (require, exports, environment_1) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function configure(aurelia) {
+        aurelia.use
+            .standardConfiguration()
+            .feature('resources');
+        aurelia.use.developmentLogging(environment_1.default.debug ? 'debug' : 'warn');
+        if (environment_1.default.testing) {
+            aurelia.use.plugin('aurelia-testing');
+        }
+        aurelia.start().then(function () { return aurelia.setRoot(); });
+    }
+    exports.configure = configure;
+});
+;
+define('resources/index',["require", "exports"], function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    function configure(config) {
+    }
+    exports.configure = configure;
+});
+
+//# sourceMappingURL=app-bundle.js.map
